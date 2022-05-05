623a9e743cdd0e63bd9f3b030c31aaf36499e3ef
silasviniciuss <codando1@gmail.com>
2022-05-05 16:58:05 -0300

    criando o html, css e instalando o bootstrap

--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculadoraComponent } from './components';
-
+import { CalculadoraService } from './services';
 
 
 @NgModule({
@@ -13,6 +13,9 @@ import { CalculadoraComponent } from './components';
   ],
   imports: [
     CommonModule
+  ],
+  providers: [
+    CalculadoraService
   ]
 })
 export class CalculadoraModule { }
